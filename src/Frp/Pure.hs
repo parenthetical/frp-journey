e11ccab25a3c056983e46609044d496d0ed3d0a2
Adriaan Leijnse <parenthetical@users.noreply.github.com>
2023-12-04 15:42:36 +0000

    Frp.Pure.Frp combinators do not refer to t

--- src/Frp/Pure.hs
+++ src/Frp/Pure.hs
@@ -6,26 +6,27 @@ import Data.Kind (Type)
 import Control.Monad.Fix
 import Control.Monad
 import Data.Align (align)
+import Control.Monad.Trans.Maybe
 
 data Pure (t :: Type)
 
 instance (Ord t, Enum t) => Frp (Pure t) where
-  newtype Behavior (Pure t) a = Behavior { sample :: t -> a }
+  newtype Behavior (Pure t) a = Behavior { at :: t -> a }
     deriving (Functor,Applicative,Monad,MonadFix)
   newtype Event (Pure t) a = Event { occurs :: t -> Maybe a }
-  type Moment (Pure t) = ((->) t)
-  mapMaybeMoment f (Event e) = Event $ \t -> ($ t) . f =<< e t
-  coincidence (Event e) = Event $ \t -> (($ t) . occurs) =<< e t
-  merge a b = Event $ \t -> align (occurs a t) (occurs b t)
+  type Moment (Pure t) = (->) t
+  mapMaybeMoment f = Event . runMaybeT . (MaybeT . f <=< MaybeT . occurs)
+  coincidence = mapMaybeMoment occurs
+  merge a b = Event $ align <$> occurs a <*> occurs b
   never = Event $ pure Nothing
-  switch b = Event $ \t -> occurs (Frp.Pure.sample b t) t
+  switch = Event . (occurs <=< sample)
 
 instance (Ord t, Enum t) => MonadMoment (Pure t) ((->) t) where
-  now t = Event (guard . (t ==))
-  sample = Frp.Pure.sample
+  now t = Event $ guard . (t ==)
+  sample = at
   hold a (Event e) from = Behavior $ \t ->
     if t <= from
     then a
     else case e (pred t) of
       Just a' -> a'
-      Nothing -> Frp.Pure.sample (hold a (Event e) from) (pred t)
+      Nothing -> sample (hold a (Event e) from) (pred t)
