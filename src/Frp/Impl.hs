32e489828e8763634ec117d79853538e82652564
Adriaan Leijnse <parenthetical@users.noreply.github.com>
2023-12-09 14:24:49 +0000

    Implementation w/o unsubscribe queue via Unsubscribe argument
    Also fixes a bug in switch unsubscribing

--- src/Frp/Impl.hs
+++ src/Frp/Impl.hs
@@ -26,9 +26,12 @@ import Data.Some
 
 data Impl
 
-type Subscriber a = Maybe a -> IO ()
+type Subscriber a = Unsubscribe -> Maybe a -> IO ()
 type Unsubscribe = IO ()
 type Invalidator = IO ()
+
+subscribe' :: Event Impl a -> (Maybe a -> IO ()) -> IO ()
+subscribe' e f = subscribe e (const f)
 
 instance Frp Impl where
   -- | Events are subscribed to with a callback which runs whenever
@@ -37,8 +40,7 @@ instance Frp Impl where
   -- that the callback will stop receiving occurrences, and wether an
   -- occurrence is known for the current frame. If the occurrence is
   -- already known the callback is not called.
-  newtype Event Impl a = Event
-    { subscribe :: Subscriber a -> IO Unsubscribe }
+  newtype Event Impl a = Event { subscribe :: Subscriber a -> IO () }
   -- | Behaviors are sampling functions which are passed an optional
   -- "invalidator". This invalidator is run when the Behavior's value
   -- might be changing.
@@ -50,36 +52,40 @@ instance Frp Impl where
   never = undefined <$ FRP.filterE (const False) rootTickE
 
   mapMaybeMoment :: (a -> Moment Impl (Maybe b)) -> Event Impl a -> Event Impl b
-  mapMaybeMoment f e = cacheEvent $ Event $ \propagate -> subscribe e $ propagate <=< fmap join . mapM f
+  mapMaybeMoment f e = cacheEvent $ Event $ \propagate -> subscribe e $ \unsubscribe -> propagate unsubscribe <=< fmap join . mapM f
 
   coincidence :: Event Impl (Event Impl a) -> Event Impl a
-  coincidence coincidenceParent = cacheEvent $ Event $ \propagate -> do
-    subscribe coincidenceParent $
-      maybe (propagate Nothing) (addToQueue unsubscribeQueueRef <=< (`subscribe` propagate))
+  coincidence coincidenceParent = cacheEvent $ Event $ \propagate' -> do
+    subscribe coincidenceParent $ \unsubscribeOuter ->
+      let propagate = propagate' unsubscribeOuter
+      in maybe (propagate Nothing) (`subscribe` \unsubscribeInner occ -> unsubscribeInner >> propagate occ)
 
   merge :: forall a b. Event Impl a -> Event Impl b -> Event Impl (These a b)
   merge a b = cacheEvent $ Event $ \propagate -> do
     aOccRef <- newIORef Nothing
     bOccRef <- newIORef Nothing
-    let doSub :: forall c. IORef (Maybe (Maybe c)) -> Event Impl c -> IO Unsubscribe
-        doSub occRef e = subscribe e $ \occ -> do
+    aUnsubscribeRef <- newIORef $ error "aUnsubscribe uninitialized"
+    bUnsubscribeRef <- newIORef $ error "bUnsubscribe uninitialized"
+    let doSub :: forall c. IORef (Maybe (Maybe c)) -> IORef Unsubscribe -> Event Impl c -> IO ()
+        doSub occRef unsubscribeRef e = subscribe e $ \unsubscribe -> unsafePerformIO $ do
+          writeIORef unsubscribeRef unsubscribe
+          pure $ \occ -> do
             writeIORef occRef . Just $ occ
             mapM_ (\occRes -> do
                       writeIORef aOccRef Nothing >> writeIORef bOccRef Nothing
-                      propagate occRes)
+                      propagate (join (readIORef aUnsubscribeRef) >> join (readIORef bUnsubscribeRef)) occRes)
               =<< liftA2 align <$> readIORef aOccRef <*> readIORef bOccRef
-    (>>) <$> doSub aOccRef a <*> doSub bOccRef b
+    doSub aOccRef aUnsubscribeRef a
+    doSub bOccRef bUnsubscribeRef b
 
   switch :: Behavior Impl (Event Impl a) -> Event Impl a
   switch (Behavior switchParent) = cacheEvent $ Event $ \propagate -> do
-    unsubscribeInnerERef <- newIORef $ error "unsubscribeInnerERef uninitialized"
-    maybeInvalidatorRef <- newIORef $ error "maybeInvalidatorRef uninitialized"
-    fix $ \f -> mdo
-      writeIORef maybeInvalidatorRef . Just $ unsubscribeInnerE >> f
-      unsubscribeInnerE <- (`subscribe` propagate)
-                           <=< runReaderT switchParent $ Just $ sequence_ =<< readIORef maybeInvalidatorRef
-      writeIORef unsubscribeInnerERef unsubscribeInnerE
-    pure (writeIORef maybeInvalidatorRef Nothing >> join (readIORef unsubscribeInnerERef))
+    maybeUnsubscribeInnerERef <- newIORef $ error "unsubscribeInnerERef uninitialized"
+    fix $ \f ->
+      runReaderT switchParent (Just $ mapM_ (>> f) =<< readIORef maybeUnsubscribeInnerERef)
+      >>= (`subscribe` \unsubscribeInnerE -> unsafePerformIO $ do
+              writeIORef maybeUnsubscribeInnerERef (Just unsubscribeInnerE)
+              pure $ propagate (writeIORef maybeUnsubscribeInnerERef Nothing >> unsubscribeInnerE))
 
 data BehaviorAssignment where
   BehaviorAssignment :: IORef a -> a -> IORef [Invalidator] -> BehaviorAssignment
@@ -90,10 +96,10 @@ instance MonadMoment Impl IO where
     invsRef <- newIORef []
     valRef <- newIORef v0
     -- Make sure not to touch 'e' eagerly, instead wait for Behavior init time.
-    addToQueue behaviorInitsRef $ void $ subscribe e $
+    addToQueue behaviorInitsRef $ void $ subscribe' e $
       mapM_ (\a -> addToQueue behaviorAssignmentsRef $ BehaviorAssignment valRef a invsRef)
-    pure $ Behavior $ ReaderT $ \invalidator -> do
-      mapM_ (addToQueue invsRef) invalidator
+    pure $ Behavior $ ReaderT $ \maybeInvalidator -> do
+      mapM_ (addToQueue invsRef) maybeInvalidator
       readIORef valRef
 
   now :: Moment Impl (Event Impl ())
@@ -120,14 +126,14 @@ managedSubscribersEvent = do
   pure
     ( Event $ \propagate -> do
         thisSubId <- atomicModifyIORef ctrRef (\i -> (succ i, i))
-        modifyIORef subscribersRef $ IntMap.insert thisSubId propagate
+        let propagate' = propagate $ do
+             old <- readIORef subscribersRef
+             unless (IntMap.member thisSubId old) $ error "managedSubscribers unsubscribed twice"
+             modifyIORef subscribersRef (IntMap.delete thisSubId)
+        modifyIORef subscribersRef $ IntMap.insert thisSubId propagate'
         -- If occRef is already Just we have to propagate on subscribe
         -- because the subscription on e has already propagated:
-        mapM_ propagate =<< readIORef occRef
-        pure $ do
-          old <- readIORef subscribersRef
-          unless (IntMap.member thisSubId old) $ error "managedSubscribers unsubscribed twice"
-          modifyIORef subscribersRef (IntMap.delete thisSubId)
+        mapM_ propagate' =<< readIORef occRef
     , \occ -> do
         writeAndScheduleClear "managedSubscribers" occRef occ
         mapM_ ($ occ) =<< readIORef subscribersRef
@@ -136,9 +142,8 @@ managedSubscribersEvent = do
 cacheEvent :: forall a. Event Impl a -> Event Impl a
 cacheEvent e = unsafePerformIO $ do
   (eCached, doPropagate) <- managedSubscribersEvent
-  void . subscribe e $ doPropagate
+  void . subscribe' e $ doPropagate
   pure eCached
-
 
 type MakeEventTrigger a = (a -> EventTrigger)
 type EventTrigger = IO ()
@@ -154,7 +159,7 @@ subscribeEvent e = do
   -- notify the user that they tried to read an event occurrence
   -- outside of a frame. Maybe subscribeEvent should return a read action which throws an exception.
   occRef :: IORef (Maybe a) <- newIORef Nothing
-  _ <- subscribe e $ mapM_ (writeAndScheduleClear "subscribeEvent" occRef)
+  _ <- subscribe' e $ mapM_ (writeAndScheduleClear "subscribeEvent" occRef)
   pure occRef
 
 runFrame :: [EventTrigger] -> IO a -> IO a
@@ -169,7 +174,6 @@ runFrame triggers program = do
       sequence_ inits
       runHoldInits
   atomicModifyIORef toClearQueueRef ([],) >>= mapM_ (\(Some (Compose occRef)) -> writeIORef occRef Nothing)
-  atomicModifyIORef unsubscribeQueueRef ([],) >>= sequence_
   atomicModifyIORef behaviorAssignmentsRef ([],)
     >>= mapM_ (\(BehaviorAssignment valRef a invalidatorsRef) -> do
                   writeIORef valRef a
@@ -184,15 +188,11 @@ writeAndScheduleClear name occRef a = do
   addToQueue toClearQueueRef (Some (Compose occRef))
 
 addToQueue :: IORef [a] -> a -> IO ()
-addToQueue q a = modifyIORef q (a:)
+addToQueue q = modifyIORef q . (:)
 
 {-# NOINLINE toClearQueueRef #-}
 toClearQueueRef :: IORef [Some (Compose IORef Maybe)]
 toClearQueueRef = unsafePerformIO $ newIORef []
-
-{-# NOINLINE unsubscribeQueueRef #-}
-unsubscribeQueueRef :: IORef [IO ()]
-unsubscribeQueueRef = unsafePerformIO $ newIORef []
 
 {-# NOINLINE behaviorAssignmentsRef #-}
 behaviorAssignmentsRef :: IORef [BehaviorAssignment]
